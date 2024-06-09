6413230363d15f8bd591a26bbb5a187d88cc20f3
Iago Luan <iagoluancampelo@gmail.com>
2024-06-09 12:20:52 -0300

    Update script.js

--- projeto_terça/js/script.js
+++ projeto_terça/js/script.js
@@ -1,15 +1,6 @@
 let btn = document.querySelector(".button");
 let qr_code_element = document.querySelector(".qr-code");
 
-function cpf(v){
-    v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-                                             //de novo (para o segundo bloco de números)
-    v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
-    return v
-}
-
 btn.addEventListener("click", ( ) => {
 let input_nome = document.querySelector("#input_nome");
 let input_matricula = document.querySelector("#input_matricula");
